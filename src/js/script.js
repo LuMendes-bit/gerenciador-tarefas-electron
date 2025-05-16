3d592a13ae2a112980f6bb246def9789b3af1c4d
Lucas Mendes <lucas.mendes.1234@hotmail.com>
2025-05-15 22:14:44 -0300

    script

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,18 @@
 function addTask() {
+    //console.log("Entrou na função addTask")
     //alert("Função de adicionar tarefa")
-    var input = document.getElementById("input-tarefa").value
-    console.log(input)
+    var input = document.getElementById("input-task")
+    var inputValue = input.value
+    input.value = ""
+    var list = document.getElementById("task-list")
+    var li =document.createElement("li")
+   //li.innerText = inputValue
+   //li.innerHTML = " Minha tarefa <button>Excluir</button> "
+   //li.innerHTML = inputValue + " <button>Excluir</button> "
+   li.innerHTML = `${inputValue} <button onclick="this.parentElement.remove()" >🗑️</button> `
+   li.onclick = function() {
+    li.className = "completed-task"
+   }
+    list.appendChild(li)
+    console.log(inputValue)
 }
